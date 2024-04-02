21cddc595d6c230f29ec425a4064a7bbae15f003
o2-forbes <oliver2.forbes@live.uwe.ac.uk>
2024-04-02 20:52:06 +0100

    26

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,18 +1,157 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject dialogueParent;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private Button option1Button;
+    [SerializeField] private Button option2Button;
+
+    [SerializeField] private float typingSpeed = 0.05f;
+    [SerializeField] private float turnSpeed = 2f;
+
+    private List<dialogueString> dialogueList;
+
+    [Header("Player")]
+    [SerializeField] private StarterAssets.FirstPersonController firstPersonController;
+    private Transform playerCamera;
+
+    private int CurrentDialogueIndex = 0;
+
+    private void Start()
     {
-        
+        dialogueParent.SetActive(false);
+        playerCamera = Camera.main.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DialogueStart(List<dialogueString> textToPrint, Transform NPC)
     {
-        
+        dialogueParent.SetActive(true);
+        firstPersonController.enabled = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Camera.main.GetComponent<Cinemachine.CinemachineBrain>().enabled = false;
+
+        StartCoroutine(TurnCameraTowardsNPC(NPC));
+
+        dialogueList = textToPrint;
+        CurrentDialogueIndex = 0;
+
+        DisableButtons();
+
+        StartCoroutine(PrintDialogue());
+    }
+
+    private void DisableButtons()
+    {
+        option1Button.interactable = false;
+        option2Button.interactable = false;
+
+        option1Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+        option2Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+    }
+
+    private IEnumerator TurnCameraTowardsNPC(Transform NPC)
+    {
+
+        Quaternion startRotation = playerCamera.rotation;
+        Quaternion targetRotation = Quaternion.LookRotation(NPC.position - playerCamera.position);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < 1f)
+        {
+            playerCamera.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime);
+            elapsedTime += Time.deltaTime * turnSpeed;
+            yield return null;
+        }
+
+        playerCamera.rotation = targetRotation;
+    }
+
+    private bool optionSelected = false;
+
+    private IEnumerator PrintDialogue()
+    {
+        while (CurrentDialogueIndex < dialogueList.Count)
+        {
+            dialogueString line = dialogueList[CurrentDialogueIndex];
+
+            line.startDialogueEvent?.Invoke();
+
+            if (line.isQuestion)
+            {
+                yield return StartCoroutine(TypeText(line.text));
+
+                option1Button.interactable = true;
+                option2Button.interactable = true;
+
+                option1Button.GetComponentInChildren<TMP_Text>().text = line.answerOption1;
+                option2Button.GetComponentInChildren<TMP_Text>().text = line.answerOption2;
+
+                option1Button.onClick.AddListener(() => HandleOptionSelected(line.option1IndexJump));
+                option2Button.onClick.AddListener(() => HandleOptionSelected(line.option2IndexJump));
+
+                yield return new WaitUntil(() => optionSelected);
+            }
+            else
+            {
+                yield return StartCoroutine(TypeText(line.text));
+            }
+
+            line.endDialogueEvent?.Invoke();
+
+            optionSelected = false;
+        }
+
+        DialogueStop();
+    }
+
+    private void HandleOptionSelected(int indexJump)
+    {
+        optionSelected = true;
+        DisableButtons();
+
+        CurrentDialogueIndex = indexJump;
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        dialogueText.text = "";
+        foreach (char letter in text.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        if (!dialogueList[CurrentDialogueIndex].isQuestion)
+        {
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        }
+
+        if (dialogueList[CurrentDialogueIndex].isEnd)
+            DialogueStop();
+
+        CurrentDialogueIndex++;
+    }
+
+    private void DialogueStop()
+    {
+        StopAllCoroutines();
+        dialogueText.text = "";
+        dialogueParent.SetActive(false);
+
+        firstPersonController.enabled = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Camera.main.GetComponent<Cinemachine.CinemachineBrain>().enabled = true;
     }
 }
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private List<dialogueString> dialogueStrings = new List<dialogueString>();
+    [SerializeField] private Transform NPCTransform;
 
-    // Update is called once per frame
-    void Update()
+    private bool hasSpoken = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player") && !hasSpoken)
+        {
+            other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings, NPCTransform);
+            hasSpoken = true;
+        }
     }
+}
+
+[System.Serializable]
+public class dialogueString
+{
+    public string text;
+    public bool isEnd;
+
+    [Header("Branch")]
+    public bool isQuestion;
+    public string answerOption1;
+    public string answerOption2;
+    public int option1IndexJump;
+    public int option2IndexJump;
+
+    [Header("Triggered Events")]
+    public UnityEvent startDialogueEvent;
+    public UnityEvent endDialogueEvent;
+
 }
